6dc97308f09390e0281dd274c05f2fa99ef9a36e
Lisa Liu <liuyj19@lenovo.com>
2019-04-28 20:22:37 +0800

    meta-lenovo: hr630: lenovo-powerctrl: install power control scripts of host
    Install the power control shell scripts into /usr/sbin, These
    scripts are for power on and power off host system.
    
    Tested:
    Run script poweron.sh could power on host system
    Run script poweroff.sh could power off host system
    
    Signed-off-by: Lisa Liu <liuyj19@lenovo.com>
    Change-Id: Ibe8b6198bd81b852968cc213efd381675e748dec

---
+++ meta-hr630/recipes-phosphor/lenovo-powerctrl/files/poweron.sh
@@ -0,0 +1,12 @@
+#"Copyright (c) 2019-present Lenovo
+#Licensed under BSD-3, see COPYING.BSD file for details."
+
+#!/bin/bash
+GPIO_BASE=$(cat /sys/devices/platform/ahb/ahb:apb/1e780000.gpio/gpio/*/base)
+GPIO_NUM=$(($GPIO_BASE + 64 + 0))
+
+echo 1 > /sys/class/gpio/gpio${GPIO_NUM}/value
+sleep 1
+echo 0 > /sys/class/gpio/gpio${GPIO_NUM}/value
+
+exit 0;
